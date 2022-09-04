1f15041fcf89955ac2eb6d3a009c6b87f30c3431
Prometheus <poligraphich@gmail.com>
2022-09-04 22:05:12 +0300

    add hw1

---
+++ example001_hw_02/Program.cs
@@ -0,0 +1,14 @@
+﻿int randomNum = new Random().Next(100,1000);
+Console.WriteLine(randomNum);
+int digitFirst = randomNum / 10;
+int digitSecond = digitFirst % 10;
+Console.WriteLine($"{digitSecond}");
+
+int FindMax(int num1, int num2)
+{
+    if(num1 > num2)
+    {
+return num1;
+    }
+return num1;
+}
