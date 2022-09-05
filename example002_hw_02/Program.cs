1e235b61e84f1c81cd096b55581fa81708ad08d3
Prometheus <poligraphich@gmail.com>
2022-09-05 19:37:05 +0300

    add v2 in 2 zadacha

---
+++ example002_hw_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+{
+   if (numberText.Length > 2)
+   {
+    Console.WriteLine("третья цифра " + numberText[2] );
+
+   }
+   else
+   {
+    Console.WriteLine("нет такого числа" );
+   }
+}
