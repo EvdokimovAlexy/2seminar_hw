b5394bb967a38a24be204f6430a13eea1e73ae24
Prometheus <poligraphich@gmail.com>
2022-09-07 18:56:44 +0300

    add example003

---
+++ example003_hw_02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число обозначающий день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+    if(number == 6 || number == 7)
+        {
+             Console.WriteLine("Выходной");
+        }
+         else if(number > 7)
+    {
+        Console.WriteLine("Вообще не то пальто");
+    }
+    else 
+    {
+        Console.WriteLine("Всем на работу");
+    }
+   
+    
